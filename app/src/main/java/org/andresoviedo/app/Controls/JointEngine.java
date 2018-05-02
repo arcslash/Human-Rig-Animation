db9bd021928cd26bf1f21109196e61d70ece2ba3
amith01994 <amith01994@gmail.com>
2018-05-02 09:42:23 +0530

    Working state solution not controllable T shaped still model

--- app/src/main/java/org/andresoviedo/app/Controls/JointEngine.java
+++ app/src/main/java/org/andresoviedo/app/Controls/JointEngine.java
@@ -34,7 +34,7 @@ public class JointEngine {
     public void changePose(){
         //change the joint according to the jointid
         float[] transmat = new float[]{-0.9408169f, 0.28628558f, -0.88139493f, 0.0f, 0.18397914f, -0.018085718f, -0.98276377f, 0.0f, -0.28463173f, -0.9579736f, -0.03565526f, 0.0f, 0.0f, 1.282774f, 0.0f, 1.0f};
-        this.currentPose.put("Lower_Leg_R",transmat);
+        this.currentPose.put(currentPose.keySet(),transmat);
 
 
 
